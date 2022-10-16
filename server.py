37261c7e04c0cfbfc84876b80890c326922344b3
clayton14 <claytoneasley77@gmail.com>
2022-10-16 18:52:51 -0400

    backend complete testing moretesting needed

--- server.py
+++ server.py
@@ -9,11 +9,17 @@ import asyncio
 from config.db import engine, SessionLocal
 from sqlalchemy.orm import session
 from thing import ThingModel
+
+
 app = FastAPI(title="Power Controll")
+
+#valid list of GPIO pins to use
+GPIOs = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+         12, 13, 16, 17, 18, 19, 20, 21,
+         22, 23, 24, 25, 26, 27]
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-
 ThingModel.metadata.create_all(bind=engine)
 
 
@@ -29,9 +35,8 @@ def get_db():
 
 class Thing(BaseModel):
     name: str
-    board_pin: int = Field(..., gt=1, lt=40)
+    board_pin: int = Field(..., gt=1, lt=30)
     status: bool = False
-
 
 
 #using depedency injection
@@ -45,7 +50,7 @@ async def create_thing(thing: Thing, db: session = Depends(get_db)):
     """
     Creat a thing model and add to database
     """
-    thing_model = ThingModel
+    thing_model = ThingModel()
     thing_model.name = thing.name
     thing_model.board_pin = thing.board_pin
     thing_model.status = thing.status
@@ -57,7 +62,7 @@ async def create_thing(thing: Thing, db: session = Depends(get_db)):
 
 @app.put("/")
 async def update_thing(id: int, thing: Thing, db: session = Depends(get_db)):
-    thing_model = db.query(ThingModel).filter(ThingModel.thing_id == id).first()
+    thing_model = db.query(ThingModel).filter(ThingModel.thing_id == id).first() # check to see if the item exisits
     if thing_model is None:
         raise HTTPException(
             status_code=404, 
@@ -71,31 +76,61 @@ async def update_thing(id: int, thing: Thing, db: session = Depends(get_db)):
     return thing
 
 
+@app.put("/status/{id}/{status}")
+async def update_status(id: int, status: bool, db:  session = Depends(get_db)):
+    thing_model = db.query(ThingModel).filter(ThingModel.thing_id == id).first() # check to see if the item exisits
+    if thing_model is None:
+        raise HTTPException(
+            status_code =  404,
+            detail=f"ID {id}: Does not exist"
+        )
+    #turn on or off the pins in the rasbperry pi
+    if (thing_model.thing_id == id):
+        thing_model.status = int(status)
+        GPIO.setup(thing_model.board_pin, GPIO.OUT)
+        GPIO.output(thing_model.board_pin, not status)
 
-# @app.delete("/")
-# async def delete_thing(id: int, thing: Thing):
+
+    db.add(thing_model)
+    db.commit()
+
+
+
+    return status
+
+
+
+
+@app.delete("/{id}")
+async def delete_thing(id: int, db: session = Depends(get_db)):
+    thing_model = db.query(ThingModel).filter(ThingModel.thing_id == id).first() # check to see if the item exisits
+    if thing_model is None:
+        raise HTTPException(
+            status_code =  404,
+            detail=f"ID {id}: Does not exist"
+        )
+    db.query(ThingModel).filter(ThingModel.thing_id == id).delete()
+    db.commit()
+ 
+    return f"{thing_model.name} has been deleted"
+
+# @app.get("/pin_test/{num}/{status}")
+# async def avivate_pin(num: int, status: int):
 #     try:
-#         obj = thing_list.pop(id)
-#         return obj
+#         GPIO.setup(num, GPIO.OUT)
+#         GPIO.output(num, status)
+#         print(num)
+#         return f"testing pin {num}"
 #     except:
-#         raise HTTPException(status_code=404, detail="Thing not found")
-
-
-@app.get("/pin_test/{num}/{status}")
-async def avivate_pin(num: int, status: int):
-    try:
-        GPIO.setup(num, GPIO.OUT)
-        GPIO.output(num, status)
-        print(num)
-        return f"testing pin {num}"
-    except:
-         raise HTTPException(status_code=500, detail="Invalid rpi pin")
-
+#          raise HTTPException(
+#             status_code=500, 
+#             detail="Invalid rpi pin"
+#         )
 
 
 
 async def main():
-    config = uvicorn.Config("server:app", port=8000, log_level="info", host="0.0.0.0", reload=True)
+    config = uvicorn.Config("server:app", port=8000, log_level="info", host="0.0.0.0")
     server = uvicorn.Server(config)
     await server.serve()
 
