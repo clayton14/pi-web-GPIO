4f353f0cabe11fbe0c6d4305d002382731d3a852
clayton14 <claytoneasley77@gmail.com>
2022-10-22 00:03:15 -0400

    testing templates

--- server.py
+++ server.py
@@ -1,25 +1,29 @@
-import imp
-from fastapi  import Depends, FastAPI, HTTPException, APIRouter
+from fastapi import Depends, FastAPI, HTTPException, Request
 import RPi.GPIO as GPIO
 from pydantic import BaseModel, Field
 from typing import List
-import pydantic
 import uvicorn
 import asyncio
 from config.db import engine, SessionLocal
 from sqlalchemy.orm import session
 from thing import ThingModel
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI(title="Power Controll")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-#valid list of GPIO pins to use
+# valid list of GPIO pins to use
 GPIOs = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
          12, 13, 16, 17, 18, 19, 20, 21,
          22, 23, 24, 25, 26, 27]
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
+
 ThingModel.metadata.create_all(bind=engine)
 
 
@@ -38,14 +42,24 @@ class Thing(BaseModel):
     board_pin: int = Field(..., gt=1, lt=30)
     status: bool = False
 
+#####################################API#####################################
 
-#using depedency injection
+
 @app.get("/")
-async def root(db: session = Depends(get_db)):
+async def root(req: Request, db: session = Depends(get_db)):
+    # # responce = db.query(ThingModel).all()
+    # return templates.TemplateResponse("index.html", {"request": req,
+    #                                                  "x": db.query(ThingModel).all()}
+    #                                   )
     return db.query(ThingModel).all()
 
 
-@app.post("/")
+# Template test
+@app.get("/test/", response_class=HTMLResponse)
+async def test(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/things/")
 async def create_thing(thing: Thing, db: session = Depends(get_db)):
     """
     Creat a thing model and add to database
@@ -60,12 +74,13 @@ async def create_thing(thing: Thing, db: session = Depends(get_db)):
     return thing
 
 
-@app.put("/")
+@app.put("/things/")
 async def update_thing(id: int, thing: Thing, db: session = Depends(get_db)):
-    thing_model = db.query(ThingModel).filter(ThingModel.thing_id == id).first() # check to see if the item exisits
+    thing_model = db.query(ThingModel).filter(
+        ThingModel.thing_id == id).first()  # check to see if the item exisits
     if thing_model is None:
         raise HTTPException(
-            status_code=404, 
+            status_code=404,
             detail="Thing not found"
         )
     thing_model.name = thing.name
@@ -78,13 +93,14 @@ async def update_thing(id: int, thing: Thing, db: session = Depends(get_db)):
 
 @app.put("/status/{id}/{status}")
 async def update_status(id: int, status: bool, db:  session = Depends(get_db)):
-    thing_model = db.query(ThingModel).filter(ThingModel.thing_id == id).first() # check to see if the item exisits
+    thing_model = db.query(ThingModel).filter(
+        ThingModel.thing_id == id).first()  # check to see if the item exisits
     if thing_model is None:
         raise HTTPException(
-            status_code =  404,
+            status_code=404,
             detail=f"ID {id}: Does not exist"
         )
-    #turn on or off the pins in the rasbperry pi
+    # turn on or off the pins in the rasbperry pi
     if (thing_model.thing_id == id):
         thing_model.status = int(status)
         GPIO.setup(thing_model.board_pin, GPIO.OUT)
@@ -96,28 +112,28 @@ async def update_status(id: int, status: bool, db:  session = Depends(get_db)):
     return status
 
 
-@app.delete("/{id}")
+@app.delete("/things/{id}")
 async def delete_thing(id: int, db: session = Depends(get_db)):
-    thing_model = db.query(ThingModel).filter(ThingModel.thing_id == id).first() # check to see if the item exisits
+    thing_model = db.query(ThingModel).filter(
+        ThingModel.thing_id == id).first()  # check to see if the item exisits
     if thing_model is None:
         raise HTTPException(
-            status_code =  404,
+            status_code=404,
             detail=f"ID {id}: Does not exist"
         )
 
     GPIO.setup(thing_model.board_pin, GPIO.OUT)
     GPIO.output(thing_model.board_pin, False)
 
-
     db.query(ThingModel).filter(ThingModel.thing_id == id).delete()
     db.commit()
- 
+
     return f"{thing_model.name} has been deleted"
 
 
-
 async def main():
-    config = uvicorn.Config("server:app", port=8000, log_level="info", host="0.0.0.0")
+    config = uvicorn.Config("server:app", port=8000,
+                            log_level="info", host="0.0.0.0")
     server = uvicorn.Server(config)
     await server.serve()
 
