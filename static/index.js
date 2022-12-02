cd9e7408fa8267b2a515a8d4f835f3b4fa557572
clayton14 <claytoneasley77@gmail.com>
2022-12-02 01:18:02 -0500

    js stuff added

--- static/index.js
+++ static/index.js
@@ -1,29 +1,56 @@
-async function update_things(){
-    return null;
-}
-
-async function create_thing(){
-    return null;
-}
-
-async function remove_thing(){
-    return null;
-}
-
-async function update_status(){
-    return null;
-}
-
-async function get_things(endpoint){
-    let response = await fetch(endpoint);
-    let data = await response.json();
-    return data;
-}
+let test_ip = "http://127.0.0.1:8000/things/"
+    async function get_things(endpoint){
+        let response = await fetch(endpoint, {
+            method: "GET"
+        })
+        .then(res => {
+            if(res.ok){
+                return res.json()
+            }else{
+                console.error("[ERROR] something is not working")
+            }
+        })
+        .then(json => {
+            return json
+        })
+        
+        // let data = await response.json();
+        // return data;
+    }
 
 
-function build_html(){
+    async function update_things(endpoint, thing_name, new_pin){
+        let response = await fetch(endpoint, {
+            method: "PUT",
+            body: JSON.stringify({
+                
+            })
+        })
+    }
+
+    async function create_thing(){
+        return null;
+    }
+
+    async function remove_thing(){
+        return null;
+    }
+
+    async function update_status(){
+        return null;
+    }
+
     
 
-}
+    function build_html(){
+        let things = get_things(test_ip)
+        console.log(typeof things)
+        document.getElementById("app").innerHTML = 
+        `
+        <p>${things}</p>
+        `
 
-console.log(get_things("http://claypower.local:8000/"))
+    }
+    build_html()
+    console.log(get_things(test_ip))
+    
